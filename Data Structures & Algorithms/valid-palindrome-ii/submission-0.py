edb270d37c5a85aa4a888cc507f1fa275d3ebf12
Fazlı Altun <32793348+fazlialtunn@users.noreply.github.com>
2026-08-21 17:23:50 +0300

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                # Delete either the left or right mismatched character
+                return (
+                    self.isPalindrome(s, left + 1, right)
+                    or self.isPalindrome(s, left, right - 1)
+                )
+
+            left += 1
+            right -= 1
+
+        return True
+
+    def isPalindrome(self, s: str, left: int, right: int) -> bool:
+        while left < right:
+            if s[left] != s[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
